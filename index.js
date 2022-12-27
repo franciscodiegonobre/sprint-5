d55f778ee7f41115b8613d6b4f900d51798f1999
francisco.nobre <franciscodiegonobre@gmail.com>
2022-12-27 16:56:56 +0100

    ejercicio 3

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 "use strict";
-var _a;
+var _a, _b, _c, _d;
 exports.__esModule = true;
 (_a = document === null || document === void 0 ? void 0 : document.getElementById("nextJoke")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", fetchJoke);
+(_b = document === null || document === void 0 ? void 0 : document.getElementById("funnyJoke")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", function (evt) { return rateJoke("funnyJoke"); });
+(_c = document === null || document === void 0 ? void 0 : document.getElementById("okJoke")) === null || _c === void 0 ? void 0 : _c.addEventListener("click", function (evt) { return rateJoke("okJoke"); });
+(_d = document === null || document === void 0 ? void 0 : document.getElementById("badJoke")) === null || _d === void 0 ? void 0 : _d.addEventListener("click", function (evt) { return rateJoke("badJoke"); });
 var joketext = document.getElementById("joketext");
 var api_url = "https://icanhazdadjoke.com";
+var reportAcudits = [];
 //headers needed to get a response from the API
 var headerApi = {
     method: "GET",
@@ -11,23 +15,40 @@ var headerApi = {
         "Accept": "application/json"
     }
 };
-//function to get the joke from the API and insert it as text in html
+//function to get the joke from the API and insert it as text in html. It also passes the fetched joke
+//to createReport function
 function fetchJoke() {
     fetch(api_url, headerApi)
         .then(function (res) { return res.json(); })
         .then(function (response) {
         joketext.textContent = "\"".concat(response.joke, "\"");
         console.log(response.joke);
+        createReport(response.joke);
     })["catch"](function (error) { return console.error("Something went wrong ->", error); });
 }
-/* const maintext = document.getElementById("maintext") */
-/* function fetchJoke() {
-    fetch(api_url, headerApi)
-        .then((res) => res.json())
-        .then(response => {
-            let paragraph = document.createElement("p")
-            paragraph.innerHTML = response.joke
-            maintext?.appendChild(paragraph)
-            console.log(response.joke)
-    }).catch(error => console.error("Something went wrong ->", error))
-    } */ 
+//function to create a joke object with the current joke and insert it in the reportAcudits array
+function createReport(joke) {
+    var jokeObj = {
+        joke: joke,
+        score: undefined,
+        date: undefined
+    };
+    reportAcudits.push(jokeObj);
+}
+//function to assign the score and the date to the last joke object, which is the one the user is 
+//currently seeing, if the user decides to vote
+function rateJoke(rate) {
+    if (rate === "funnyJoke") {
+        reportAcudits[reportAcudits.length - 1].score = 3;
+        reportAcudits[reportAcudits.length - 1].date = new Date().toISOString();
+    }
+    if (rate === "okJoke") {
+        reportAcudits[reportAcudits.length - 1].score = 2;
+        reportAcudits[reportAcudits.length - 1].date = new Date().toISOString();
+    }
+    if (rate === "badJoke") {
+        reportAcudits[reportAcudits.length - 1].score = 1;
+        reportAcudits[reportAcudits.length - 1].date = new Date().toISOString();
+    }
+    console.log(reportAcudits);
+}
--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
 export {};
 
 document?.getElementById("nextJoke")?.addEventListener("click", fetchJoke);
+document?.getElementById("funnyJoke")?.addEventListener("click", (evt) => rateJoke("funnyJoke"));
+document?.getElementById("okJoke")?.addEventListener("click", (evt) => rateJoke("okJoke"));
+document?.getElementById("badJoke")?.addEventListener("click", (evt) => rateJoke("badJoke"));
 
 const joketext = document.getElementById("joketext")
 const api_url: string = "https://icanhazdadjoke.com"
+type jokeObject = {
+    joke: string,
+    score: (undefined | number),
+    date: (undefined | string)
+};
+let reportAcudits: jokeObject[] = []
 
 //headers needed to get a response from the API
 const headerApi: {} = {
@@ -13,12 +22,46 @@ const headerApi: {} = {
     }
 }
 
-//function to get the joke from the API and insert it as text in html
+//function to get the joke from the API and insert it as text in html. It also passes the fetched joke
+//to createReport function
 function fetchJoke() {
 fetch(api_url, headerApi)
     .then((res) => res.json())
     .then(response => {
         joketext!.textContent = `"${response.joke}"`
         console.log(response.joke)
+        createReport(response.joke)
 }).catch(error => console.error("Something went wrong ->", error))
 }
+
+//function to create a joke object with the current joke and insert it in the reportAcudits array
+function createReport(joke: string) {
+    let jokeObj = {
+            joke: joke,
+            score: undefined,
+            date: undefined
+    }
+
+    reportAcudits.push(jokeObj)
+}
+
+//function to assign the score and the date to the last joke object, which is the one the user is 
+//currently seeing, if the user decides to vote
+function rateJoke(rate: string) {
+    if (rate === "funnyJoke") {
+        reportAcudits[reportAcudits.length - 1].score = 3 
+        reportAcudits[reportAcudits.length - 1].date = new Date().toISOString() 
+    }
+
+    if (rate === "okJoke") {
+        reportAcudits[reportAcudits.length - 1].score = 2 
+        reportAcudits[reportAcudits.length - 1].date = new Date().toISOString() 
+    }
+
+    if (rate === "badJoke") {
+        reportAcudits[reportAcudits.length - 1].score = 1 
+        reportAcudits[reportAcudits.length - 1].date = new Date().toISOString() 
+    }
+    console.log(reportAcudits)
+}
+
